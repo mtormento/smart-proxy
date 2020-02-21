be9cf1483f73d122d68d2bc9030924060b3707a1
Marco Tormento <marco.tormento@vipera.com>
2020-02-21 16:49:46 +0100

    Renamed and fixed some issues

--- src/smart-proxy.js
+++ src/smart-proxy.js
@@ -3,7 +3,7 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 
 var port = '7000'
-var defaultHost = 'http://gl.co:5050';
+var defaultHost = 'http://localhost:5050';
 var alternativeHost = 'http://www.ansa.it:80';
 var alternativeJsonKey = 'device';
 var alternativeJsonValue = 'fbk';
@@ -32,58 +32,68 @@ log.info('=================');
 log.info('Smart Proxy 0.1.0');
 log.info('=================');
 log.info('Listening on port ' + port);
-var proxy = httpProxy.createProxyServer({});
+
+const proxy = httpProxy.createProxyServer({
+    preserveHeaderKeyCase: true
+});
+proxy.on('proxyReq', function(proxyReq, req, res, options) {
+    proxyReq.write(req.body);
+});
+
+proxy.on('proxyRes', function(proxyRes, req, res) {
+    log.info('[' + req.device + '] Proxyed ' + req.url + ' request to ' + req.targetHost);
+});
+
+proxy.on('error', function (err, req, res, target) {
+    log.error('[' + req.device + '] Proxying ' + req.url + ' request to ' + req.targetHost + ' failed: ' + JSON.stringify(err));
+
+    res.writeHead(500, {
+        'Content-Type': 'application/json'
+    });
+
+    res.end(JSON.stringify({
+        smartProxyError: err
+    }));
+});
 
 log.info('default host: ' + defaultHost);
 log.info('alternative host: ' + alternativeHost);
 
-var server = http.createServer(function(req, res) {
+const server = http.createServer(function(req, res) {
 
-    var isJson = req.headers['content-type'];
-    isJson = isJson ? isJson.includes('application/json') : false;
-    var body = "";
+    req.body = ""
 
     req.on('readable', function() {
         var data = req.read();
         if (data != null) {
-            body += data;
+            req.body += data;
         }
     });
     req.on('end', function() {
-        var device = 'unknown';
-        var targetHost = defaultHost;
+        req.device = 'unknown';
+        req.targetHost = defaultHost;
+        var isJson = req.headers['content-type'];
+        isJson = isJson ? isJson.includes('application/json') : false;
         if (isJson) {
             try {
-                obj = JSON.parse(body);
+                obj = JSON.parse(req.body);
                 const value = obj[alternativeJsonKey];
-                if (value.startsWith(alternativeJsonValue)) {
-                    targetHost  = alternativeHost
+                if (value) {
+                    req.device = value;
+                }
+                if (req.device.startsWith(alternativeJsonValue)) {
+                    req.targetHost  = alternativeHost
                 }
             } catch (e) {
             }
         }
 
         proxy.web(req, res, { 
-            target: targetHost
-        });
-        log.info('[' + device + '] Proxying ' + req.url + ' request to ' + targetHost);
-
-        proxy.on('proxyReq', function(proxyReq, req, res, options) {
-            proxyReq.write(body);
+            target: req.targetHost
         });
 
-        proxy.on('error', function (err, req, res) {
-            log.error('[' + device + '] Proxying ' + req.url + ' request to ' + targetHost + ' failed: ' + JSON.stringify(err));
-
-            res.writeHead(500, {
-                'Content-Type': 'application/json'
-            });
-
-            res.end(JSON.stringify({
-                smartProxyError: err
-            }));
-        });
     });
+
 });
 
 server.listen(port);
